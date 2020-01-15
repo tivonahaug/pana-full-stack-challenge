16d847a9a1dcb3829995f51e6bc46000865e147f
Michael Hottman <hottmanmichael@gmail.com>
2020-01-15 11:29:53 -0700

    Styling updates

--- react-challenge/src/components/Instructions/index.js
+++ react-challenge/src/components/Instructions/index.js
@@ -1,35 +1,41 @@
 import React from 'react';
 
+const Subheader = ({ children }) => <h2>{children}</h2>;
+
 export default function Instructions() {
   return (
-    <div className="row">
+    <div
+      className="row"
+      style={{ maxWidth: '1000px', display: 'flex', justifyContent: 'center' }}
+    >
       <div className="col-xs-12">
-        <h2>Pana Frontend Code Challenge</h2>
+        <h1>Pana Frontend Code Challenge</h1>
       </div>
       <div className="col-xs-12">
-        <h3>Overview</h3>
-        <b>
-          The goal is to finish implementing a sample front-end of one of our
-          products, Pana Flight Search.
-        </b>
+        <Subheader>Overview</Subheader>
+        <p>
+          The goal of this challenge is to implement a sample front-end of one
+          of our products, Pana Flight Search.
+        </p>
         <p>
           During this challenge, you'll work through several "milestones" that
-          have increasing difficulty. Each milestone will test a different part
-          of your knowledge of frontend Javascript and React. We've built out
-          the basic scaffolding for you, you'll need to build out the rest.
+          have increasing levels difficulty. Each milestone will test a
+          different part of your knowledge of frontend Javascript and React.
+          We've built out the basic scaffolding for you, you'll need to build
+          out the rest.
         </p>
       </div>
       <div className="col-xs-12">
-        <h3>Milestones</h3>
+        <Subheader>Milestones</Subheader>
         <ol>
           <li>
             <h4>Display the Data</h4>
             <ul>
               <li>
                 The basic layout is already built out for each flight, you'll
-                just need to finish up a few of the "Placeholder" sections. You
-                can refer to the following rough mock to see what the end result
-                will look like.
+                just need to finish up a few of the <code>Placeholder</code>{' '}
+                sections. You can refer to the following rough mock to see what
+                the end result will look like.
               </li>
               <img
                 src="/assets/flight_search.png"
